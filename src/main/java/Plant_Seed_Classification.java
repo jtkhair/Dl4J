03db2f2ea5d51f28c773df848bedf3fe95e98d9a
Training <jauhari.tahir@gmail.com>
2020-08-28 16:39:06 +0800

    Master

--- src/main/java/Plant_Seed_Classification.java
+++ src/main/java/Plant_Seed_Classification.java
@@ -1,5 +1,16 @@
+//Example of plant seed classification using CNN
+
+import org.slf4j.Logger;
+
 public class Plant_Seed_Classification {
-    public static void main(String[] args) {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Plant_Seed_Classification.class);
+    private static String dataDir;
+    private static String downloadLink;
+
+
+
+
+    public static void main(String[] args) throws Exception{
 
     }
 
